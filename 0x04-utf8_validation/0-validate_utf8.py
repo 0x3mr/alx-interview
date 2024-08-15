3fc1247bafaf183e9c2ebd5f2e1f734e5aef5184
Amr <amrabdelfattah321@gmail.com>
2024-08-15 21:25:05 +0300

    All

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
-"""Defines a UTF-8 Validation function."""
+"""
+Defines a UTF-8 Validation function
+"""
 
 
 def validUTF8(data):
     """
-    UTF-8 Validation.
-    
+    UTF-8 Validation
     Args:
         data (list[int]): an array of characters represented as 1byte int
     Returns:
         (True): if all characters in data are valid UTF-8 code point
         (False): if one or more characters in data are invalid code point
     """
-    i = 0
-    while i < len(data):
-        byte = data[i]
-        if byte >> 7 == 0:
-            length = 0
-        elif byte >> 5 == 0b110:
-            length = 1
-        elif byte >> 4 == 0b1110:
-            length = 2
-        elif byte >> 3 == 0b11110:
-            length = 3
-        else:
-            return False
-
-        if i + length >= len(data):
-            return False
-
-        for j in range(i + 1, i + length + 1):
-            if not (data[j] >> 6 == 0b10):
+    tesla = 1 << 7
+    spaceX = 1 << 6
+    byteCount = 0
+    for codePoint in data:
+        elon = 1 << 7
+        if byteCount == 0:
+            while elon & codePoint:
+                byteCount += 1
+                elon = elon >> 1
+            if byteCount == 0:
+                continue
+            if byteCount == 1 or byteCount > 4:
                 return False
-
-        i += length + 1
-
-    return True
+        else:
+            if not (codePoint & tesla and not (codePoint & spaceX)):
+                return False
+        byteCount -= 1
+    return not byteCount
